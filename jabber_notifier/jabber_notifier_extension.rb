255859ff7a7eebbf1e34066cbd574a7fc21c993f
Michel Loiseleur <mloiseleur@linagora.com>
2008-07-18 14:35:47 +0200

    fix loading of extension

--- jabber_notifier/jabber_notifier_extension.rb
+++ jabber_notifier/jabber_notifier_extension.rb
@@ -1,6 +1,6 @@
 require_dependency 'application'
 
-class JabberNotifierExtension < Tosca:Extension
+class JabberNotifierExtension < Tosca::Extension
   version "1.0"
   description "Allows to receive instant notification when a request is submitted."
   url "http://tosca-project.net"
