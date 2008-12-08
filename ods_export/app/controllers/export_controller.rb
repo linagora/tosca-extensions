e524ad69ebc4a206f1f65c32678cc84b5979f048
Rémy-Christophe Schermesser <git@schermesser.com>
2008-12-08 18:25:52 +0100

    Migrate plugins to match the new names

--- ods_export/app/controllers/export_controller.rb
+++ ods_export/app/controllers/export_controller.rb
@@ -124,7 +124,7 @@ class ExportController < ApplicationController
   def compute_issues(type, options_generate)
     columns = [ 'id', 'softwares_name', 'clients_name', 'severites_name',
       'created_on_formatted', 'socle', 'updated_on_formatted', 'resume',
-      'statuts_name', 'typeissues_name'
+      'statuts_name', 'issuetypes_name'
     ]
     options= { :order => 'issues.created_on', :conditions => flash[:conditions],
       :select => Issue::SELECT_LIST, :joins => Issue::JOINS_LIST,
