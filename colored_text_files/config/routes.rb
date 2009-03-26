9080a517e659b37cedfe55be53162e291e746989
Loiseleur Michel <mloiseleur@linagora.com>
2009-03-26 16:58:43 +0100

    fix definitely ctf

--- colored_text_files/config/routes.rb
+++ colored_text_files/config/routes.rb
@@ -1,2 +1,3 @@
-named_route 'uv_attachment', "/attachments/:id/uv/",
-  :controller => 'attachments', :action => 'uv', :method => :get
+# named_route 'uv_attachment', "/attachments/:id/uv/",
+#  :controller => 'attachments', :action => 'uv', :method => :get
+resources :attachments, :member => { :uv => :get }
